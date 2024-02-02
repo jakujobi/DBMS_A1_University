dd9ee0183c7028787cba6b4fca94af04d5d63b67
John <john@jakujobi.com>
2024-02-01 20:44:07 -0600

    Add comments for SQL queries for university database scenarios

---
+++ h.sql
@@ -0,0 +1,56 @@
+
+
+
+-- ! _____________________________________________________________________________________________________________________________________________________________________________
+-- ! _____________________________________________________________________________________________________________________________________________________________________________
+/*
+
+
+
+*/
+
+/* 
+Consider the university database schema
+Write an SQL query for each of the following scenarios:
+*/
+
+/* 
+a. Find the ID and name of each instructor in the Finance department
+*/
+SELECT
+
+/* 
+b. Find the ID and name of each instructor in a department located in the Painter building
+*/
+
+/* 
+c. Find the ID and name of each student who has taken at least one course in the Music department
+*/
+
+/* 
+d. Find the ID and name of each student who has taken at least one course section in the year 2017
+*/
+
+/* 
+e. List the ID and name of those instructors who are not from Biology dept
+*/
+
+/* 
+f. List the courses_id, semester, year of all courses that were taught in Fall 2017 or in Spring 2018
+*/
+
+/* 
+g. List the information of all courses which include ‘Co’ as a substring in its title
+*/
+
+/* 
+h. Find the titles of all courses in the Comp. Sci. dept that have 4 credits
+*/
+
+/* 
+i. Find the id and name of the students from the ‘Comp. Sci.’ dept whose name has 5 characters
+*/
+
+/* 
+j. Find the information of the students from ‘Comp. Sci.’ and ‘Elec. Eng.’ Department whose total credits are above 55
+*/
