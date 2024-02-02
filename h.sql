d2a6e6e9f2509470441099f76ad14aa2a6ab8d00
John <john@jakujobi.com>
2024-02-01 21:30:16 -0600

    Solved a, e, g, h

--- h.sql
+++ h.sql
@@ -282,16 +282,27 @@ insert into prereq values ('EE-181', 'PHY-101');
 Consider the university database schema
 Write an SQL query for each of the following scenarios:
 */
+SELECT *
+FROM instructor;
 
 /* 
 a. Find the ID and name of each instructor in the Finance department
 */
-
+SELECT ID, name
+FROM instructor
+WHERE dept_name = 'Finance';
 
 
 /* 
 b. Find the ID and name of each instructor in a department located in the Painter building
 */
+
+
+
+
+-- View students
+SELECT *
+FROM student;
 
 /* 
 c. Find the ID and name of each student who has taken at least one course in the Music department
@@ -305,6 +316,11 @@ d. Find the ID and name of each student who has taken at least one course sectio
 /* 
 e. List the ID and name of those instructors who are not from Biology dept
 */
+SELECT ID, name
+FROM instructor
+WHERE dept_name <> 'Finance';
+
+
 
 /* 
 f. List the courses_id, semester, year of all courses that were taught in Fall 2017 or in Spring 2018
@@ -313,14 +329,29 @@ f. List the courses_id, semester, year of all courses that were taught in Fall 2
 /* 
 g. List the information of all courses which include ‘Co’ as a substring in its title
 */
+SELECT *
+FROM course
+WHERE title LIKE '%Co%';
+
 
 /* 
 h. Find the titles of all courses in the Comp. Sci. dept that have 4 credits
 */
+SELECT title
+FROM course
+WHERE credits = 4;
+
+
 
 /* 
 i. Find the id and name of the students from the ‘Comp. Sci.’ dept whose name has 5 characters
 */
+SELECT id, name
+FROM student
+WHERE dept_name = 'Comp. Sci.'
+  AND LEN(name)=5;
+
+
 
 /* 
 j. Find the information of the students from ‘Comp. Sci.’ and ‘Elec. Eng.’ Department whose total credits are above 55
