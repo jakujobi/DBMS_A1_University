f2cb85c8b01fe635c457228e2fcc70d0b81bf712
Izak Benitez-Lopez <izak.benitezlopez@jacks.sdstate.edu>
2024-02-03 16:58:25 -0600

    Update h.sql

--- h.sql
+++ h.sql
@@ -279,6 +279,7 @@ insert into prereq values ('EE-181', 'PHY-101');
 */
 
 /* 
+/* 
 Consider the university database schema
 Write an SQL query for each of the following scenarios:
 */
@@ -296,62 +297,68 @@ WHERE dept_name = 'Finance';
 /* 
 b. Find the ID and name of each instructor in a department located in the Painter building
 */
-SELECT ID, name FROM instructor
-WHERE dept_name IN (SELECT dept_name FROM department WHERE building = 'Painter');
+
+SELECT DISTINCT instructor.*
+FROM instructor
+JOIN department ON instructor.dept_name = department.dept_name
+WHERE department.building = 'Painter';
 
 
-/* 
-c. Find the ID and name of each student who has taken at least one course in the Music department
-*/
-SELECT ID, name FROM student
-WHERE ID IN
-    (SELECT ID from takes WHERE course_id IN
-        (SELECT course_id FROM course WHERE dept_name = 'Music'));
+-- -- View students
+SELECT id
+FROM student;
 
+-- /* 
+-- c. Find the ID and name of each student who has taken at least one course in the Music department
+-- */
+SELECT distinct student.id, student.name
+FROM student
+JOIN takes on student.id = takes.id
+WHERE course_id LIKE '%MU%';
 
-/* 
-d. Find the ID and name of each student who has taken at least one course section in the year 2017
-*/
-SELECT ID, name FROM student
-WHERE ID IN (SELECT ID FROM takes WHERE year = 2017);
+-- /* 
+-- d. Find the ID and name of each student who has taken at least one course section in the year 2017
+-- */
+SELECT distinct student.id, student.name
+FROM student
+JOIN takes on student.id = takes.id
+WHERE year = 2017
 
-
-/* 
-e. List the ID and name of those instructors who are not from Biology dept
-*/
+-- /* 
+-- e. List the ID and name of those instructors who are not from Biology dept
+-- */
 SELECT ID, name
 FROM instructor
-WHERE dept_name <> 'Finance';
+WHERE dept_name <> 'Biology';
 
-
-
-/* 
-f. List the courses_id, semester, year of all courses that were taught in Fall 2017 or in Spring 2018
-*/
-SELECT course_id, semester, year FROM course
-WHERE semester IN ('Fall', 'Spring')
-  AND year IN (2017, 2018);
-
-/* 
-g. List the information of all courses which include ‘Co’ as a substring in its title
-*/
+-- /* 
+-- f. List the courses_id, semester, year of all courses that were taught in Fall 2017 or in Spring 2018
+-- */
+SELECT distinct course_id, semester, year
+FROM section
+WHERE 
+	(semester = 'Fall' AND year = 2017)
+	OR
+	(semester = 'Spring' and year = 2018);
+-- /* 
+-- g. List the information of all courses which include ‘Co’ as a substring in its title
+-- */
 SELECT *
 FROM course
 WHERE title LIKE '%Co%';
 
 
-/* 
-h. Find the titles of all courses in the Comp. Sci. dept that have 4 credits
-*/
+-- /* 
+-- h. Find the titles of all courses in the Comp. Sci. dept that have 4 credits
+-- */
 SELECT title
 FROM course
-WHERE credits = 4;
+WHERE credits = 4 AND dept_name = 'Comp. Sci.'
 
 
-
-/* 
-i. Find the id and name of the students from the ‘Comp. Sci.’ dept whose name has 5 characters
-*/
+-- /* 
+-- i. Find the id and name of the students from the ‘Comp. Sci.’ dept whose name has 5 characters
+-- */
 SELECT id, name
 FROM student
 WHERE dept_name = 'Comp. Sci.'
@@ -360,9 +367,9 @@ WHERE dept_name = 'Comp. Sci.'
 
 
 
-/* 
-j. Find the information of the students from ‘Comp. Sci.’ and ‘Elec. Eng.’ Department whose total credits are above 55
-*/
+-- /* 
+-- j. Find the information of the students from ‘Comp. Sci.’ and ‘Elec. Eng.’ Department whose total credits are above 55
+-- */
 SELECT *
 FROM student
 WHERE dept_name IN ('Comp. Sci.','Elec. Eng.')
